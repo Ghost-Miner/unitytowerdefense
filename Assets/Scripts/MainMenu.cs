46a4b66b48b8e2720e82a346b28c137e774301d8
Ghost-Miner <Jnovy35@gmail.com>
2021-12-15 18:15:25 +0100

    feas

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.IO;
 
 public class MainMenu : MonoBehaviour
 {
@@ -11,16 +12,41 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private TMP_Text versionText;
 
     public SceneTransition sceneTransition;
+
+    private bool levelFinished;
+    private string dataFolder;
+    private string saveFile;
     #endregion
 
     private void Start()
     {
+        dataFolder = Application.dataPath + "/Game data" + "/";
+        saveFile = "Tutorial.json";
+
         versionText.text = GameManager.versionName;
+
+        ReadJson();
     }
 
     public void LoadScene (string sceneName)
     {
         sceneTransition.ChangeScene(sceneName);
+    }
+
+    public void PlayGame ()
+    {
+        string scene;
+
+        if  (levelFinished)
+        {
+            scene = "Level select";
+        }
+        else
+        {
+            scene = "Tutorial 1";
+        }
+
+        sceneTransition.ChangeScene(scene);
     }
 
     public void OpenLink (string link)
@@ -50,5 +76,29 @@ public class MainMenu : MonoBehaviour
         anim.Play(newState);
 
         currentState = newState;
+    }
+
+    private void ReadJson()
+    {
+        if (!File.Exists(dataFolder + saveFile))
+        {
+            Debug.Log("File " + saveFile + " doesnt exist");
+
+            levelFinished = false;
+        }
+
+        string json = File.ReadAllText(dataFolder + saveFile);
+
+        LevelData levelData = JsonUtility.FromJson<LevelData>(json);
+        Debug.Log(gameObject.name + ": " + json);
+
+        levelFinished = levelData.finished;
+    }
+
+    // List of settings values
+    public class LevelData
+    {
+        public bool unlocked;
+        public bool finished;
     }
 }
